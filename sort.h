0c75b574bbb3d9ad23398b29cd0c17c6d434a7f3
mauroxcf <maurox03@gmail.com>
2020-10-19 02:30:23 +0000

    fixing betty header

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-# ifndef SORTING_ALGORITHM
-# define SORTING_ALGORITHM
+# ifndef SORT_H
+# define SORT_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
